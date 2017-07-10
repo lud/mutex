2c8afa7e47b40949bba158cf114aa6995c9fc6f7
niahoo <dev@ooha.in>
2017-07-11 01:21:10 +0200

    Added package information for hex

--- mix.exs
+++ mix.exs
@@ -4,6 +4,10 @@ defmodule Mutex.Mixfile do
   def project do
     [
       app: :mutex,
+      description: """
+      This package implements a simple mutex as a GenServer. It allows to await
+      locked keys and handles locking multiple keys without deadlocks.
+      """,
       version: "1.0.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
@@ -14,30 +18,26 @@ defmodule Mutex.Mixfile do
         main: "readme",
         extras: ["README.md"],
       ],
+      package: package(),
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:ex_doc, "~> 0.14", only: :dev, runtime: false},
       {:dogma, "~> 0.1.15", only: :dev},
+    ]
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      maintainers: ["niahoo osef <dev@ooha.in>"],
     ]
   end
 end
