2ef22e86c9d5176210975e548dde49db8ca6bc9e
lud <ludovic@demblans.com>
2022-11-29 14:59:10 +0100

    new version 1.3.2

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Mutex.Mixfile do
       This package implements a simple mutex as a GenServer. It allows to await
       locked keys and handles locking multiple keys without deadlocks.
       """,
-      version: "1.3.1",
+      version: "1.3.2",
       elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
